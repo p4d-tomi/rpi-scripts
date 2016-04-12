c92e69920cbb6b867b391df2b1d5ca182b0984c7
tmigone <tomasmigone@gmail.com>
2016-04-12 17:34:04 -0300

    Added rc.local & install.sh

---
+++ installers/install.sh
@@ -0,0 +1,17 @@
+
+# Install Node.js v4.0.0
+curl -s https://raw.githubusercontent.com/p4d-tomi/rpi-scripts/master/installers/install-node.sh | sudo sh
+
+# Install boot-notification script
+curl -s https://raw.githubusercontent.com/p4d-tomi/rpi-scripts/master/installers/install-boot.notification.sh | sh
+
+
+
+
+# Replace /etc/rc.local
+sudo cp /etc/rc.local /etc/rc.local.bkp
+sudo curl -s https://raw.githubusercontent.com/p4d-tomi/rpi-scripts/master/installers/rc.local -o /etc/rc.local
+
+
+# Reboot
+sudo reboot

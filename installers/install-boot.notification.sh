788c4f796aa5cd88601bccae1b7ee966a366d216
p4d-tomi <tomas@p4d.com.ar>
2016-04-12 16:30:53 -0300

    Update install-boot.notification.sh

--- installers/install-boot.notification.sh
+++ installers/install-boot.notification.sh
@@ -3,10 +3,9 @@
 # This script installs boot-notification.js for ARM into ~/scripts
 # Run it like this: "curl -s link-to-github.sh | sudo sh"
 
-cd ~
-
-if [ ! -d "rpi-scripts" ]
+if [ ! -d "~/rpi-scripts" ]
 then
+    cd ~
     git clone https://github.com/p4d-tomi/rpi-scripts.git
 else
     cd rpi-scripts
